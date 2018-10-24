ece9538b58e617d1bad2c27cb573896041405cba
Doug Pilcher <dpilcher.dev@gmail.com>
2018-10-23 21:47:37 -0500

    toggle filter

--- pages/articles.js
+++ pages/articles.js
@@ -37,20 +37,13 @@ const Checkbox = ({ id, name, checked = true, onChange }) => (
       onClick={(e) => onChange(e, id)}
     >
       {name}</button>
-    {/* <label htmlFor={catKey}>
-      <input
-        type="checkbox"
-        name={catKey}
-        checked={checked}
-        onChange={onChange}
-      />
-      {name}</label> */}
   </Fragment>
 )
 
 class Articles extends Component {
   state = {
-    filters: {}
+    filters: {},
+    showFilters: true
   }
   static async getInitialProps() {
     const posts = await fetch(`${process.env.HOST_URL || '/'}api/posts`).then(res => res.json());
@@ -62,10 +55,8 @@ class Articles extends Component {
   componentDidMount() {
     let filters = {};
     this.props.categories.forEach(cat => {
-      // post.categories.forEach(cat => {
       if (cat._id in filters) return;
       filters[cat._id] = false;
-      // })
     });
     this.setState({ filters })
   }
@@ -73,11 +64,75 @@ class Articles extends Component {
   toggleChecked = (e, id) => {
     console.log('toggling');
     e.preventDefault();
-    // const name = e.target.name;
-    // const checked = e.target.checked;
     let filters = { ...this.state.filters };
     filters[id] = !filters[id];
     this.setState({ filters });
+  }
+
+  toggleFilters = e => {
+    e.preventDefault();
+    // this.formRef.dataset.expand = !this.formRef.dataset.expand;
+    // let showFilters = this.state.showFilters;
+    // showFilters = !showFilters;
+    // this.setState({ showFilters });
+
+    var section = this.formRef;
+    var isCollapsed = section.getAttribute('data-collapsed') === 'true';
+
+    if (isCollapsed) {
+      this.expandSection(section)
+      section.setAttribute('data-collapsed', 'false')
+    } else {
+      this.collapseSection(section)
+    }
+  }
+
+  collapseSection = (element) => {
+    // get the height of the element's inner content, regardless of its actual size
+    var sectionHeight = element.scrollHeight;
+
+    // temporarily disable all css transitions
+    var elementTransition = element.style.transition;
+    element.style.transition = '';
+
+    // on the next frame (as soon as the previous style change has taken effect),
+    // explicitly set the element's height to its current pixel height, so we 
+    // aren't transitioning out of 'auto'
+    requestAnimationFrame(function () {
+      element.style.height = sectionHeight + 'px';
+      element.style.transition = elementTransition;
+
+      // on the next frame (as soon as the previous style change has taken effect),
+      // have the element transition to height: 0
+      requestAnimationFrame(function () {
+        element.style.height = 0 + 'px';
+      });
+    });
+
+    // mark the section as "currently collapsed"
+    element.setAttribute('data-collapsed', 'true');
+  }
+
+  expandSection = (element) => {
+    // get the height of the element's inner content, regardless of its actual size
+    var sectionHeight = element.scrollHeight;
+
+    // have the element transition to the height of its inner content
+    element.style.height = sectionHeight + 'px';
+
+    function func(e) {
+      // remove this event listener so it only gets triggered once
+      element.removeEventListener('transitionend', func);
+
+      // remove "height" from the element's inline styles, so it can return to its initial value
+      element.style.height = null;
+    }
+
+    // when the next css transition finishes (which should be the one we just triggered)
+    element.addEventListener('transitionend', func);
+
+    // mark the section as "currently not collapsed"
+    element.setAttribute('data-collapsed', 'false');
   }
 
   render() {
@@ -105,22 +160,27 @@ class Articles extends Component {
           letter-spacing: 1px;
           margin: 1rem 10px;
         }
-        #form__filters {
+        #filters__form {
           display: grid;
           grid-template-columns: repeat(auto-fill, minmax(7rem, 1fr));
           justify-items: center;
           align-items: center;
-          padding-bottom: 1rem;
           grid-gap: 10px;
-          border-bottom: 1px solid #EB3E34;
-          margin: 1rem 0;
-          height: 0;
-          display: none;
+          margin-bottom: 1rem;
         }
-        #form__filters[data-expand='true'] {
-          display: grid;
-          min-height: 10px;
+        .filters__form__wrapper {
+          border-bottom: 1px solid #EB3E34;
+          margin-bottom: 1rem;
           height: auto;
+          overflow: hidden;
+          transition: height 150ms ease-out;
+        }
+        .filters__title {
+          background: none;
+          border: none;
+        }
+        .filters__title:focus {
+          outline: none;
         }
         @media(min-width: 576px) {
           h1 {
@@ -135,17 +195,19 @@ class Articles extends Component {
         }
         `}</style>
         <h1>Articles</h1>
-        <h3>Filters</h3>
-        <form id="form__filters" data-expand={true}>
-          {categories.filter(cat1 => posts.some(post => post.categories.some(cat2 => cat1._id === cat2._id))).map(cat => (
-            <Checkbox
-              key={cat._id}
-              id={cat._id}
-              name={cat.name}
-              checked={this.state.filters[cat._id]}
-              onChange={this.toggleChecked} />
-          ))}
-        </form>
+        <h3><button className="filters__title" onClick={this.toggleFilters}><FontAwesomeIcon icon={faFilter} size="sm" /> Filters</button></h3>
+        <div className="filters__form__wrapper" data-collapse={true} ref={el => this.formRef = el}>
+          <form id="filters__form">
+            {categories.filter(cat1 => posts.some(post => post.categories.some(cat2 => cat1._id === cat2._id))).map(cat => (
+              <Checkbox
+                key={cat._id}
+                id={cat._id}
+                name={cat.name}
+                checked={this.state.filters[cat._id]}
+                onChange={this.toggleChecked} />
+            ))}
+          </form>
+        </div>
         <ArticleCardGrid
           posts={Object.values(this.state.filters).some(val => val) ?
             posts.filter(post => post.categories.some(cat => this.state.filters[cat._id])) :
