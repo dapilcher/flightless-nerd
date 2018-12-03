d306db571ae2e01b28137a8574a5d19cd0b169e5
Doug Pilcher <dpilcher.dev@gmail.com>
2018-12-03 13:58:01 -0600

    Fix analytics exports

--- analytics.js
+++ analytics.js
@@ -3,25 +3,27 @@ import getConfig from 'next/config';
 
 const { publicRuntimeConfig } = getConfig();
 
-exports.initGA = () => {
+const initGA = () => {
   // console.log('GA init')
   ReactGA.initialize(publicRuntimeConfig.gaTracker);
 }
 
-exports.logPageView = () => {
+const logPageView = () => {
   // console.log('Logging pageview for ${window.location.pathname}')
   ReactGA.set({ page: window.location.pathname })
   ReactGA.pageview(window.location.pathname)
 }
 
-exports.logEvent = (category = '', action = '') => {
+const logEvent = (category = '', action = '') => {
   if (category && action) {
     ReactGA.event({ category, action })
   }
 }
 
-exports.logException = (description = '', fatal = false) => {
+const logException = (description = '', fatal = false) => {
   if (description) {
     ReactGA.exception({ description, fatal })
   }
 }
+
+export { initGA, logPageView, logEvent, logException };
