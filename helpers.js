dc8812572e2a8f892a0b9ec9cf5501b9921de292
Doug Pilcher <dpilcher.dev@gmail.com>
2018-12-07 17:51:42 -0600

    Fixed relative date

--- helpers.js
+++ helpers.js
@@ -1,8 +1,7 @@
 const moment = require("moment");
 
 exports.toRelativeTime = timestamp => {
-	const splitDate = timestamp.split("T")[0];
-	const relativeTime = moment(splitDate, "YYYY-MM-DD").fromNow();
+	const relativeTime = moment(timestamp).fromNow();
 	return relativeTime;
 };
 
