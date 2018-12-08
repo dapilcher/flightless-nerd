c492807e06e14afa982958dfaea53fb867e01369
Doug Pilcher <dpilcher.dev@gmail.com>
2018-12-08 10:13:39 -0600

    Prefetch if article is featured

--- components/ArticleCard.js
+++ components/ArticleCard.js
@@ -110,7 +110,7 @@ const ArticleCard = ({ post }) => (
 				}
 			}
 		`}</style>
-		<Link href={`/post?id=${post._id}`}>
+		<Link href={`/post?id=${post._id}`} prefetch={post.isFeatured}>
 			<div className="article-card pointer">
 				<div className="article-card-img-container">
 					{post.image ?
