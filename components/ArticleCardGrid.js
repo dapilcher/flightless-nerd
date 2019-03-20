ccb80136a0354704952b0648ca39cd958fe73bdc
Doug Pilcher <dpilcher.dev@gmail.com>
2019-03-20 09:38:52 -0500

    Fix card grid filtering

--- components/ArticleCardGrid.js
+++ components/ArticleCardGrid.js
@@ -1,58 +1,63 @@
 import { Fragment } from "react";
 import ArticleCard from "./ArticleCard";
 
-const ArticleCardGrid = ({ posts, count = 6 }) => (
-	<Fragment>
-		<style jsx>{`
-			.post-cards {
-				padding: 1rem 0;
-				display: grid;
-				grid-template-columns: repeat(auto-fill, minmax(300px, 1fr));
-				grid-gap: 1rem;
-				justify-content: center;
-			}
-			.post-card:not(:last-child) {
-				border-bottom: 1px solid #eb3e34;
-			}
-			#no-posts {
-				font-family: Raleway;
-				font-size: 1.2rem;
-				font-weight: 400;
-				margin: 0 10px 1rem 10px;
-				margin-bottom: 1rem;
-			}
-			@media (min-width: 768px) {
+const ArticleCardGrid = ({ posts, count = 6 }) => {
+	if (count > 0) {
+		posts = posts.slice(0, count);
+	}
+	return (
+		<Fragment>
+			<style jsx>{`
 				.post-cards {
+					padding: 1rem 0;
+					display: grid;
 					grid-template-columns: repeat(auto-fill, minmax(300px, 1fr));
+					grid-gap: 1rem;
+					justify-content: center;
 				}
-			}
-			@media (min-width: 685px) {
 				.post-card:not(:last-child) {
-					border-bottom: none;
+					border-bottom: 1px solid #eb3e34;
 				}
-			}
-			@media (min-width: 568px) {
 				#no-posts {
-					margin: 0 0 1rem 0;
+					font-family: Raleway;
+					font-size: 1.2rem;
+					font-weight: 400;
+					margin: 0 10px 1rem 10px;
+					margin-bottom: 1rem;
 				}
-			}
-		`}</style>
-		<div className="post-cards__container">
-			{posts.length > 0 ? (
-				<div className="post-cards">
-					{posts.slice(0, count).map((post, i) => {
-						return (
-							<div className="post-card" key={i}>
-								<ArticleCard post={post} />
-							</div>
-						);
-					})}
-				</div>
-			) : (
-				<h4 id="no-posts">No posts available...</h4>
-			)}
-		</div>
-	</Fragment>
-);
+				@media (min-width: 768px) {
+					.post-cards {
+						grid-template-columns: repeat(auto-fill, minmax(300px, 1fr));
+					}
+				}
+				@media (min-width: 685px) {
+					.post-card:not(:last-child) {
+						border-bottom: none;
+					}
+				}
+				@media (min-width: 568px) {
+					#no-posts {
+						margin: 0 0 1rem 0;
+					}
+				}
+			`}</style>
+			<div className="post-cards__container">
+				{posts.length > 0 ? (
+					<div className="post-cards">
+						{posts.map((post, i) => {
+							return (
+								<div className="post-card" key={i}>
+									<ArticleCard post={post} />
+								</div>
+							);
+						})}
+					</div>
+				) : (
+					<h4 id="no-posts">No posts available...</h4>
+				)}
+			</div>
+		</Fragment>
+	);
+};
 
 export default ArticleCardGrid;
--- pages/articles.js
+++ pages/articles.js
@@ -1,207 +1,232 @@
-import { Component, Fragment } from 'react';
-import Head from 'next/head';
-import fetch from 'isomorphic-unfetch';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faAngleDown } from '@fortawesome/free-solid-svg-icons';
-import ArticleCardGrid from '../components/ArticleCardGrid';
+import { Component, Fragment } from "react";
+import Head from "next/head";
+import fetch from "isomorphic-unfetch";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faAngleDown } from "@fortawesome/free-solid-svg-icons";
+import ArticleCardGrid from "../components/ArticleCardGrid";
 
 const Checkbox = ({ id, name, checked = true, onChange }) => (
-  <Fragment>
-    <style jsx>{`
-    .filters__button {
-      background-color: #eee;
-      color: #333;
-      font-family: Montserrat;
-      font-size: 1.2rem;
-      border-radius: 1rem 1rem 1rem 0;
-      border: 3px solid #D2251B;
-      padding: 0.2rem 0.5rem;
-      outline: none;
-      margin: 0;
-    }
-    .filters__button:hover {
-      transform: scale(1.03);
-      box-shadow: 1px 1px 5px rgba(0,0,0,0.5);
-    }
-    .filters__button:focus {
-      outline: none;
-    }
-    .filters__button[data-checked='true'] {
-      background-color: #EB3E34;
-      color: #eee;
-    }
-    `}</style>
-    <button
-      className="filters__button"
-      data-id={id}
-      data-checked={checked}
-      onClick={(e) => onChange(e, id)}
-    >
-      {name}</button>
-  </Fragment>
-)
+	<Fragment>
+		<style jsx>{`
+			.filters__button {
+				background-color: #eee;
+				color: #333;
+				font-family: Montserrat;
+				font-size: 1.2rem;
+				border-radius: 1rem 1rem 1rem 0;
+				border: 3px solid #d2251b;
+				padding: 0.2rem 0.5rem;
+				outline: none;
+				margin: 0;
+			}
+			.filters__button:hover {
+				transform: scale(1.03);
+				box-shadow: 1px 1px 5px rgba(0, 0, 0, 0.5);
+			}
+			.filters__button:focus {
+				outline: none;
+			}
+			.filters__button[data-checked="true"] {
+				background-color: #eb3e34;
+				color: #eee;
+			}
+		`}</style>
+		<button
+			className="filters__button"
+			data-id={id}
+			data-checked={checked}
+			onClick={e => onChange(e, id)}
+		>
+			{name}
+		</button>
+	</Fragment>
+);
 
 class Articles extends Component {
-  state = {
-    filters: {},
-    showFilters: true
-  }
-  static async getInitialProps() {
-    const posts = await fetch(`${process.env.HOST_URL || '/'}api/posts`).then(res => res.json());
-    const categories = await fetch(`${process.env.HOST_URL || '/'}api/categories`).then(res => res.json());
+	state = {
+		filters: {},
+		showFilters: true
+	};
+	static async getInitialProps() {
+		const posts = await fetch(`${process.env.HOST_URL || "/"}api/posts`).then(
+			res => res.json()
+		);
+		const categories = await fetch(
+			`${process.env.HOST_URL || "/"}api/categories`
+		).then(res => res.json());
 
-    return { posts, categories };
-  }
+		return { posts, categories };
+	}
 
-  componentDidMount() {
-    let filters = {};
-    this.props.categories.forEach(cat => {
-      if (cat._id in filters) return;
-      filters[cat._id] = false;
-    });
-    this.setState({ filters })
-  }
+	componentDidMount() {
+		let filters = {};
+		this.props.categories.forEach(cat => {
+			if (cat._id in filters) return;
+			filters[cat._id] = false;
+		});
+		this.setState({ filters });
+	}
 
-  toggleChecked = (e, id) => {
-    console.log('toggling');
-    e.preventDefault();
-    let filters = { ...this.state.filters };
-    filters[id] = !filters[id];
-    this.setState({ filters });
-  }
+	toggleChecked = (e, id) => {
+		console.log("toggling");
+		e.preventDefault();
+		let filters = { ...this.state.filters };
+		filters[id] = !filters[id];
+		this.setState({ filters });
+	};
 
-  toggleFilters = e => {
-    e.preventDefault();
+	toggleFilters = e => {
+		e.preventDefault();
 
-    let section = this.formRef;
-    let icon = this.iconRef;
-    let isCollapsed = section.getAttribute('data-collapsed') === 'true';
+		let section = this.formRef;
+		let icon = this.iconRef;
+		let isCollapsed = section.getAttribute("data-collapsed") === "true";
 
-    if (isCollapsed) {
-      this.expandSection(section)
-    } else {
-      this.collapseSection(section)
-    }
-  }
+		if (isCollapsed) {
+			this.expandSection(section);
+		} else {
+			this.collapseSection(section);
+		}
+	};
 
-  collapseSection = (element) => {
-    let sectionHeight = element.scrollHeight;
+	collapseSection = element => {
+		let sectionHeight = element.scrollHeight;
 
-    let elementTransition = element.style.transition;
-    element.style.transition = '';
+		let elementTransition = element.style.transition;
+		element.style.transition = "";
 
-    requestAnimationFrame(function () {
-      element.style.height = sectionHeight + 'px';
-      element.style.transition = elementTransition;
+		requestAnimationFrame(function() {
+			element.style.height = sectionHeight + "px";
+			element.style.transition = elementTransition;
 
-      requestAnimationFrame(function () {
-        element.style.height = 0 + 'px';
-      });
-    });
+			requestAnimationFrame(function() {
+				element.style.height = 0 + "px";
+			});
+		});
 
-    element.setAttribute('data-collapsed', 'true');
-  }
+		element.setAttribute("data-collapsed", "true");
+	};
 
-  expandSection = (element) => {
-    let sectionHeight = element.scrollHeight;
+	expandSection = element => {
+		let sectionHeight = element.scrollHeight;
 
-    element.style.height = sectionHeight + 'px';
+		element.style.height = sectionHeight + "px";
 
-    function func(e) {
-      element.removeEventListener('transitionend', func);
+		function func(e) {
+			element.removeEventListener("transitionend", func);
 
-      element.style.height = null;
-    }
+			element.style.height = null;
+		}
 
-    element.addEventListener('transitionend', func);
+		element.addEventListener("transitionend", func);
 
-    element.setAttribute('data-collapsed', 'false');
-  }
+		element.setAttribute("data-collapsed", "false");
+	};
 
-  render() {
-    const { posts, categories } = this.props;
-    return (
-      <Fragment>
-        <Head>
-          <title>{`Flightless Nerd - Articles`}</title>
-        </Head>
-        <style jsx>{`
-        margin-top: 1rem;
-        h1 {
-          color: #333;
-          font-family: Bangers;
-          font-size: 3rem;
-          letter-spacing: 3px;
-          margin: 0 10px;
-          text-shadow: 0 0 20px rgba(0,0,0,0.1);
-        }
-        h3 {
-          color: #333;
-          font-family: Montserrat;
-          font-weight: 500;
-          font-size: 1.2rem;
-          letter-spacing: 1px;
-          margin: 1rem 10px;
-        }
-        #filters__form {
-          display: grid;
-          grid-template-columns: repeat(auto-fill, minmax(7rem, 1fr));
-          justify-items: center;
-          align-items: center;
-          grid-gap: 10px;
-          margin-bottom: 1rem;
-        }
-        .filters__form__wrapper {
-          border-bottom: 1px solid #EB3E34;
-          margin-bottom: 1rem;
-          height: auto;
-          overflow: hidden;
-          transition: height 150ms ease-out;
-        }
-        .filters__title {
-          background: none;
-          border: none;
-        }
-        .filters__title:focus {
-          outline: none;
-        }
-        @media(min-width: 576px) {
-          h1 {
-            margin: 0;
-          }
-          h3 {
-            margin: 0;
-          }
-          form {
-            margin: 0;
-          }
-        }
-        `}</style>
-        <h1>Articles</h1>
-        <h3>
-          <button className="filters__title" onClick={this.toggleFilters}>
-            <span ref={el => this.iconRef = el}><FontAwesomeIcon icon={faAngleDown} size="sm" /></span>
-            {' Filters'}
-          </button>
-        </h3>
-        <div className="filters__form__wrapper" ref={el => this.formRef = el} style={{ height: '0px' }} data-collapsed="true">
-          <form id="filters__form">
-            {categories.filter(cat1 => posts.some(post => post.categories.some(cat2 => cat1._id === cat2._id))).map(cat => (
-              <Checkbox
-                key={cat._id}
-                id={cat._id}
-                name={cat.name}
-                checked={this.state.filters[cat._id]}
-                onChange={this.toggleChecked} />
-            ))}
-          </form>
-        </div>
-        <ArticleCardGrid
-          posts={Object.values(this.state.filters).some(val => val) ?
-            posts.filter(post => post.categories.some(cat => this.state.filters[cat._id])) :
-            posts} />
-      </Fragment>
-    )
-  }
+	render() {
+		const { posts, categories } = this.props;
+		return (
+			<Fragment>
+				<Head>
+					<title>{`Flightless Nerd - Articles`}</title>
+				</Head>
+				<style jsx>{`
+					margin-top: 1rem;
+					h1 {
+						color: #333;
+						font-family: Bangers;
+						font-size: 3rem;
+						letter-spacing: 3px;
+						margin: 0 10px;
+						text-shadow: 0 0 20px rgba(0, 0, 0, 0.1);
+					}
+					h3 {
+						color: #333;
+						font-family: Montserrat;
+						font-weight: 500;
+						font-size: 1.2rem;
+						letter-spacing: 1px;
+						margin: 1rem 10px;
+					}
+					#filters__form {
+						display: grid;
+						grid-template-columns: repeat(auto-fill, minmax(7rem, 1fr));
+						justify-items: center;
+						align-items: center;
+						grid-gap: 10px;
+						margin-bottom: 1rem;
+					}
+					.filters__form__wrapper {
+						border-bottom: 1px solid #eb3e34;
+						margin-bottom: 1rem;
+						height: auto;
+						overflow: hidden;
+						transition: height 150ms ease-out;
+					}
+					.filters__title {
+						background: none;
+						border: none;
+					}
+					.filters__title:focus {
+						outline: none;
+					}
+					@media (min-width: 576px) {
+						h1 {
+							margin: 0;
+						}
+						h3 {
+							margin: 0;
+						}
+						form {
+							margin: 0;
+						}
+					}
+				`}</style>
+				<h1>Articles</h1>
+				<h3>
+					<button className="filters__title" onClick={this.toggleFilters}>
+						<span ref={el => (this.iconRef = el)}>
+							<FontAwesomeIcon icon={faAngleDown} size="sm" />
+						</span>
+						{" Filters"}
+					</button>
+				</h3>
+				<div
+					className="filters__form__wrapper"
+					ref={el => (this.formRef = el)}
+					style={{ height: "0px" }}
+					data-collapsed="true"
+				>
+					<form id="filters__form">
+						{categories
+							.filter(cat1 =>
+								posts.some(post =>
+									post.categories.some(cat2 => cat1._id === cat2._id)
+								)
+							)
+							.map(cat => (
+								<Checkbox
+									key={cat._id}
+									id={cat._id}
+									name={cat.name}
+									checked={this.state.filters[cat._id]}
+									onChange={this.toggleChecked}
+								/>
+							))}
+					</form>
+				</div>
+				<ArticleCardGrid
+					count={0}
+					posts={
+						Object.values(this.state.filters).some(val => val)
+							? posts.filter(post =>
+									post.categories.some(cat => this.state.filters[cat._id])
+							  )
+							: posts
+					}
+				/>
+			</Fragment>
+		);
+	}
 }
-export default Articles
+export default Articles;
