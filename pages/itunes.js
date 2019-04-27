5c310b590d055068d536271dc6eb26f3f7a40e74
Doug Pilcher <dpilcher.dev@gmail.com>
2019-04-27 16:12:45 -0500

    redirect /itunes to /podcast

--- pages/itunes.js
+++ pages/itunes.js
@@ -1,68 +1,69 @@
 import { Component, Fragment } from "react";
-import Head from "next/head";
-import NextSeo from "next-seo";
-import Link from "next/link";
+// import Head from "next/head";
+// import NextSeo from "next-seo";
+// import Link from "next/link";
 import getConfig from "next/config";
 import getAnalytics from "../utils/getAnalytics";
-import Button from "../components/Button";
-import { FaArrowRight, FaArrowLeft } from "react-icons/fa";
-const { publicRuntimeConfig: envars } = getConfig();
+// import Button from "../components/Button";
+// import { FaArrowRight, FaArrowLeft } from "react-icons/fa";
+// const { publicRuntimeConfig: envars } = getConfig();
 
-const analytics = getAnalytics();
+// const analytics = getAnalytics();
 
-const seoConfig = {
-	title: `The Flightless Nerd Podcast | Flightless Nerd`,
-	description:
-		"Flightless Nerd is a community for people who love video game news, reviews, and blogs. Top ten lists every Friday.",
-	openGraph: {
-		type: "website",
-		locale: "en_US",
-		url: `https://flightlessnerd.com/itunes`,
-		title: `The Flightless Nerd Podcast | Flightless Nerd`,
-		description:
-			"Flightless Nerd is a community for people who love video game news, reviews, and blogs. Top ten lists every Friday.",
-		defaultImageWidth: 917,
-		defaultImageHeight: 921,
-		images: [
-			{
-				url:
-					"https://flightlessnerd.com/static/images/austrich_podcast_1280.jpg",
-				width: 1280,
-				height: 720,
-				alt: "The Flightless Nerd Podcast"
-			},
-			{
-				url:
-					"https://flightlessnerd.com/static/images/Austrich_circle_cropped.png",
-				width: 917,
-				height: 921,
-				alt: "Flightless Nerd"
-			}
-		],
-		site_name: "Flightless Nerd"
-	},
-	twitter: {
-		site: "@FlightlessNews",
-		handle: "@FlightlessNews",
-		cardType: "summary_large_image"
-	}
-};
+// const seoConfig = {
+// 	title: `The Flightless Nerd Podcast | Flightless Nerd`,
+// 	description:
+// 		"Flightless Nerd is a community for people who love video game news, reviews, and blogs. Top ten lists every Friday.",
+// 	openGraph: {
+// 		type: "website",
+// 		locale: "en_US",
+// 		url: `https://flightlessnerd.com/itunes`,
+// 		title: `The Flightless Nerd Podcast | Flightless Nerd`,
+// 		description:
+// 			"Flightless Nerd is a community for people who love video game news, reviews, and blogs. Top ten lists every Friday.",
+// 		defaultImageWidth: 917,
+// 		defaultImageHeight: 921,
+// 		images: [
+// 			{
+// 				url:
+// 					"https://flightlessnerd.com/static/images/austrich_podcast_1280.jpg",
+// 				width: 1280,
+// 				height: 720,
+// 				alt: "The Flightless Nerd Podcast"
+// 			},
+// 			{
+// 				url:
+// 					"https://flightlessnerd.com/static/images/Austrich_circle_cropped.png",
+// 				width: 917,
+// 				height: 921,
+// 				alt: "Flightless Nerd"
+// 			}
+// 		],
+// 		site_name: "Flightless Nerd"
+// 	},
+// 	twitter: {
+// 		site: "@FlightlessNews",
+// 		handle: "@FlightlessNews",
+// 		cardType: "summary_large_image"
+// 	}
+// };
 
 class Itunes extends Component {
 	componentWillMount() {
 		console.log("mounted");
 		if (typeof window !== "undefined") {
-			console.log("window!");
-			if (/iPhone|iPad|iPod/.test(navigator.userAgent) && !window.MSStream) {
-				analytics.logEvent("Redirect", "iTunes landing page auto redirect");
-				window.location = envars.podcastItunesUrl;
-			}
+			// if (/iPhone|iPad|iPod/.test(navigator.userAgent) && !window.MSStream) {
+			// 	analytics.logEvent("Redirect", "iTunes landing page auto redirect");
+			// 	window.location = envars.podcastItunesUrl;
+			// }
+			window.location = "https://flightlessnerd.com/podcast";
 		}
 	}
 	render() {
 		return (
 			<Fragment>
-				<Head>
+				<h1>Redirecting</h1>
+				{/* <Head>
 					<title>The Flightless Nerd Podcast</title>
 				</Head>
 				<NextSeo config={seoConfig} />
@@ -131,7 +132,7 @@ class Itunes extends Component {
 							users
 						</em>
 					</p>
-				</div>
+				</div> */}
 			</Fragment>
 		);
 	}
