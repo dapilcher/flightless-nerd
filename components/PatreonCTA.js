edcca7d94c71c12e6fb8628aae7c47a9de102ea9
Doug Pilcher <dpilcher.dev@gmail.com>
2019-08-07 19:11:50 -0500

    Update patreon cta styles

--- components/PatreonCTA.js
+++ components/PatreonCTA.js
@@ -1,42 +1,17 @@
 import React from "react";
 import styled from "styled-components";
 import Button from "./Button";
-import { FaPatreon } from "react-icons/fa";
+import { FaPatreon, FaAd, FaMoneyBillWave } from "react-icons/fa";
 
 const PatreonSection = styled.section`
 	display: grid;
 	grid-template-columns: 1fr;
-	grid-template-rows: 1fr auto;
+	grid-template-rows: auto 1fr 1fr auto;
 	align-items: center;
 	margin: 2rem 10px;
-	background-color: ${props => props.theme.yellow};
-	background: linear-gradient(
-		to bottom,
-		${props => props.theme.yellow},
-		rgba(230, 207, 31, 0.05)
-	);
-	border-radius: 1rem 1rem 1rem 0;
 	font-size: 1.2rem;
-
-	@media (min-width: 768px) {
-		background: linear-gradient(
-			to right,
-			${props => props.theme.yellow},
-			rgba(230, 207, 31, 0.05)
-		);
-		margin: 2rem 0;
-		grid-template-columns: 1fr auto;
-		grid-template-rows: 1fr;
-	}
-	@media (min-width: 1200px) {
-		grid-template-columns: 3fr 2fr;
-	}
-`;
-
-const PatreonLeft = styled.section`
-	padding: 1.5rem 2rem;
-	color: ${props => props.theme.black};
 	h2 {
+		margin: 2rem auto;
 		font-family: Montserrat;
 		text-transform: uppercase;
 		background: linear-gradient(
@@ -53,10 +28,29 @@ const PatreonLeft = styled.section`
 			}
 		}
 	}
+	@media (min-width: 768px) {
+		margin: 2rem 0;
+	}
+`;
+
+const PatreonOne = styled.section`
+	padding: 1.5rem 2rem;
+	margin: 0.5rem 1rem;
+	display: grid;
+	grid-template-columns: 1fr;
+	grid-template-rows: auto 1fr;
+	color: ${props => props.theme.black};
+	background-color: ${props => props.theme.yellow};
+	background: linear-gradient(
+		to bottom,
+		${props => props.theme.yellow},
+		rgba(230, 207, 31, 0)
+	);
+	border-radius: 1rem 1rem 1rem 0;
 	.cta-text {
-		font-size: 1rem;
-		line-height: 1.5;
+		font-size: 1.1rem;
 		margin: 0;
+		order: 2;
 	}
 	.drop:first-letter {
 		font-size: 1.5rem;
@@ -67,56 +61,156 @@ const PatreonLeft = styled.section`
 	.brand-new {
 		color: ${props => props.theme.red};
 	}
-
-	@media (min-width: 768) {
+	.icon__container {
+		order: 1;
+		width: 100%;
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		margin: auto;
+	}
+	.icon {
+		color: ${props => props.theme.red};
+		font-size: 4rem;
+		margin: 1rem;
+	}
+	@media (min-width: 768px) {
+		grid-template-columns: 2fr 1fr;
+		grid-template-rows: 1fr;
+		background: linear-gradient(
+			to right,
+			${props => props.theme.yellow},
+			rgba(230, 207, 31, 0)
+		);
+		margin: 0.5rem 0;
 		.cta-text {
 			font-size: 1.2rem;
+			order: 1;
+			padding: 1rem;
+		}
+		.icon__container {
+			order: 2;
+		}
+		.icon {
+			font-size: 6rem;
 		}
 	}
 `;
-const PatreonRight = styled.section`
+
+const PatreonTwo = styled.section`
+	display: grid;
+	grid-template-columns: 1fr;
+	grid-template-rows: auto 1fr;
+	padding: 1.5rem 2rem;
+	margin: 0.5rem 1rem;
+	color: ${props => props.theme.black};
+	background-color: ${props => props.theme.yellow};
+	background: linear-gradient(
+		to bottom,
+		${props => props.theme.yellow},
+		rgba(230, 207, 31, 0)
+	);
+	border-radius: 1rem 1rem 1rem 0;
+	.cta-text {
+		font-size: 1.1rem;
+		margin: 0;
+		order: 2;
+	}
+	.drop:first-letter {
+		font-size: 1.5rem;
+		padding: 0;
+		margin: 0;
+		color: ${props => props.theme.red};
+	}
+	.brand-new {
+		background: ${props => props.theme.red};
+		padding: 0 5px;
+		border-radius: 3px 3px 3px 0;
+		color: ${props => props.theme.white};
+	}
+	.icon__container {
+		order: 1;
+		width: 100%;
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		margin: auto;
+	}
+	.icon {
+		color: ${props => props.theme.red};
+		font-size: 4rem;
+		margin: 1rem;
+	}
+	@media (min-width: 768px) {
+		grid-template-columns: 1fr 2fr;
+		grid-template-rows: 1fr;
+		background: linear-gradient(
+			to left,
+			${props => props.theme.yellow},
+			rgba(230, 207, 31, 0)
+		);
+		margin: 0.5rem 0;
+		.cta-text {
+			font-size: 1.2rem;
+			padding: 1rem;
+		}
+		.icon {
+			font-size: 6rem;
+		}
+	}
+`;
+const PatreonThree = styled.section`
 	display: flex;
 	justify-content: center;
 	align-items: center;
 	padding: 1rem 2rem;
+	margin: 0.5rem auto;
 `;
 
 const PatreonCTA = () => {
 	return (
 		<PatreonSection>
-			<PatreonLeft>
-				<h2>
-					<span className="letter initial">H</span>
-					<span className="letter post">e</span>
-					<span className="letter post">y</span>
-					<span className="letter space"> </span>
-					<span className="letter initial">N</span>
-					<span className="letter post">e</span>
-					<span className="letter post">r</span>
-					<span className="letter post">d</span>
-					<span className="letter post">s</span>
-					<span className="letter punctuation">!</span>
-				</h2>
+			<h2>
+				<span className="letter initial">H</span>
+				<span className="letter post">e</span>
+				<span className="letter post">y</span>
+				<span className="letter space"> </span>
+				<span className="letter initial">N</span>
+				<span className="letter post">e</span>
+				<span className="letter post">r</span>
+				<span className="letter post">d</span>
+				<span className="letter post">s</span>
+				<span className="letter punctuation">!</span>
+			</h2>
+			<PatreonOne>
 				<p className="cta-text drop">
 					Flightless Nerd is committed to providing an enjoyable,
-					distraction-free experience. Other sites monetize your visits by
-					selling virtual billboard space on their webpage. We, on the other
-					hand, know how much we ourselves hate seeing ads everywhere and
-					deliver an experience that we ourselves would enjoy. Problem is: we
-					got bills. If you enjoy your experience at Flightless Nerd and want to
-					pitch in to help see us continue our hard work, consider heading to
-					our <span className="brand-new">brand new</span> Patreon and becoming
-					a Patron. We are eternally grateful for all your support!
+					distraction-free experience for our readers. Other sites monetize your
+					visits by selling virtual billboard space on their webpage. We, on the
+					other hand, know how much readers hate seeing ads everywhere and want
+					to deliver an experience that we ourselves would enjoy.
 				</p>
-			</PatreonLeft>
-			<PatreonRight>
-				<Button
-					href="https://patreon.com/flightlessnerd"
-					style={{ fontSize: "1.2rem" }}
-				>
+				<section className="icon__container">
+					<FaAd className="icon" />
+				</section>
+			</PatreonOne>
+			<PatreonTwo>
+				<section className="icon__container">
+					<FaMoneyBillWave className="icon" />
+				</section>
+				<p className="cta-text drop">
+					Problem is: we got bills. If you enjoy your experience at Flightless
+					Nerd and want to pitch in to help see us continue our hard work,
+					consider heading to our <span className="brand-new">brand new</span>{" "}
+					Patreon and becoming a Patron. We are eternally grateful for all your
+					support!
+				</p>
+			</PatreonTwo>
+			<PatreonThree>
+				<Button theme="blue" href="https://patreon.com/flightlessnerd">
 					Become a patron <FaPatreon />
 				</Button>
-			</PatreonRight>
+			</PatreonThree>
 		</PatreonSection>
 	);
 };
